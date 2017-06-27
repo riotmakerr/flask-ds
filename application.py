3139a28a2f9477f6385a96e887be2a35a5134ffb
Unknown <kevinrbhasin@gmail.com>
2017-06-27 14:53:37 -0400

    ui merge

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from application import db
 # from application.models import Data
-from application.forms import EnterDBInfo, RetrieveDBInfo
+from application.forms import EmployeeInfo
 from templates.models import results
 from creds import APPLICATION_SECRET_KEY
 from compute import compute
@@ -11,17 +11,49 @@ application.debug=True
 # need to set from location
 application.secret_key = APPLICATION_SECRET_KEY
 
+@application.route('/prediction_app/api/v1.0/pred', methods=['POST'])
+def get_prediction():
+    var1 = request.json['employee_satisfaction_level']
+    var2 = request.json['last_evaluation_score']
+    var3 = request.json['number_projects_performed']
+    var4 = request.json['average_monthly_hours_worked']
+    var5 = request.json['tenure_years']
+    var6 = request.json['work_accident']
+    if "n" in var6:
+        var6 = 0
+    else:
+        var6 = 1
+    var7 = request.json['promotion_in_last_5years']
+    if "n" in var7:
+        var7 = 0
+    else:
+        var7 = 1
+    input_row = [var1, var2, var3, var4, var5, var6, var7, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+    result = str(compute(input_row))
+    return result
+
 @application.route('/', methods=['GET', 'POST'])
 @application.route('/index', methods=['GET', 'POST'])
 def index():
-    form1 = EnterDBInfo(request.form)
-    form2 = RetrieveDBInfo(request.form)
+    form1 = EmployeeInfo(request.form)
 
     if request.method == 'POST' and form1.validate():
-        test_row = [float(form1.dbNotes.data), 0.53, 2, 157, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+        form1.employee_name.data
+        form1.s_level.data
+        form1.eval_score.data
+        form1.number_projects_performed.data
+        form1.average_monthly_hours_worked.data
+        form1.tenure.data
+        wa = 0
+        if form1.work_accident.data:
+            wa = 1
+        p5 = 0
+        if form1.promotion_in_last_5years.data:
+            p5 = 1
+        test_row = [float(form1.s_level.data), float(form1.eval_score.data), form1.number_projects_performed.data, form1.average_monthly_hours_worked.data, form1.tenure.data, wa, p5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
             # test_row = [0.38, 0.53, 2, 157, 3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
 
-        chosen_one = results('joe', float(compute(test_row)))
+        chosen_one = results(form1.employee_name.data, float(compute(test_row)))
         result = [chosen_one]
         #db.session.add(chosen_one)
         #db.session.commit()
@@ -34,12 +66,12 @@ def index():
         # result = db.engine.execute("SELECT * FROM results WHERE employee_name = '" + str(form1.dbNotes.data) + "'")
         # return render_template('results.html', results=result, num_return='1')
     # change this
-    elif request.method == 'POST':
-        result = db.engine.execute("SELECT * FROM results ORDER BY leave_chance DESC")
-        return render_template('results.html', result=result)
+    # elif request.method == 'POST':
+    #     result = db.engine.execute("SELECT * FROM results ORDER BY leave_chance DESC")
+    #     return render_template('results.html', result=result)
 
 
-    return render_template('index.html', form1=form1, form2=form2)
+    return render_template('index.html', form1=form1)
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
--- application/forms.py
+++ application/forms.py
@@ -1,8 +1,15 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, validators
+from wtforms import TextField, validators, IntegerField, BooleanField, DecimalField, SelectField
+from wtforms.fields.html5 import DecimalRangeField
 
-class EnterDBInfo(Form):
-    dbNotes = TextField(label='Items to add to DB', description="db_enter", validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])    
-
-class RetrieveDBInfo(Form):
-    numRetrieve = TextField(label='Number of DB Items to Get', description="db_get", validators=[validators.required(), validators.Regexp('^\d{1}$',message=u'Enter a number between 1 and 10')])
+class EmployeeInfo(Form):
+    employee_name = TextField(label='Employee Name',  validators=[validators.required(), validators.Length(min=0, max=128, message=u'Enter 128 characters or less')])
+    s_level = DecimalRangeField(label='Satisfaction Level', default = 0, places=2)
+    eval_score = DecimalRangeField(label='Evaluation Score', default = 0, places=2)
+    number_projects_performed = IntegerField(label='Number of Projects Performed', default = 1)
+    average_monthly_hours_worked = IntegerField(label='Average Monthly Hours Worked', default = 160)
+    tenure = DecimalField(label='Tenure', default=1.52)
+    work_accident = BooleanField(label='Work Accident?')
+    promotion_in_last_5years = BooleanField(label='Promotion in Last 5 Years')
+    salary = SelectField(label='Salary', choices=[('low','Low'),('medium','Medium'),('high','High')])
+    department = SelectField(label='Department', choices=[('dep_IT', 'IT'),('dep_accounting', 'Accounting'),('dep_hr', 'HR'), ('dep_management', 'Management'), ('dep_marketing', 'Marketing'),('dep_product_mng', 'Product Management'),('dep_sales', 'Sales'),('dep_support', 'General Support'),('dep_technical', 'Tech Support'),('dep_RandD', 'Other')])
